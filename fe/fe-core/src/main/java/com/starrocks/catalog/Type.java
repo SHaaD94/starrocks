5e3887f64359693da7d941717f5b0da26b45018a
Amonpongitsara, Jenwit <jenwit.amonpongitsara@agoda.com>
2024-08-02 13:16:18 +0700

    add alias timestamp to support datetime

--- fe/fe-core/src/main/java/com/starrocks/catalog/Type.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Type.java
@@ -212,6 +212,7 @@ public abstract class Type implements Cloneable {
                     .put("STRING", Type.DEFAULT_STRING)
                     .put("INTEGER", Type.INT)
                     .put("UNSIGNED", Type.INT)
+                    .put("TIMESTAMP". Type.DATETIME)
                     .putAll(SUPPORT_SCALAR_TYPE_LIST.stream()
                             .collect(Collectors.toMap(x -> x.getPrimitiveType().toString(), x -> (ScalarType) x)))
                     .build();
